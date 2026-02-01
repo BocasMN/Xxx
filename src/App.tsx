7d7c0fe045150418557e6d81e3c39ee249829fd0
BocasMN <Ricardonnoeiras@gmail.com>
2026-02-01 03:00:01 +0100

    Add main App component with Header and AnalysisDisplay

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from './components/Header'
+import AnalysisDisplay from './components/AnalysisDisplay'
+
+export default function App() {
+  return (
+    <div style={{ background:'#0b0f1a', minHeight:'100vh', color:'#fff' }}>
+      <Header />
+      <AnalysisDisplay />
+    </div>
+  )
+}
